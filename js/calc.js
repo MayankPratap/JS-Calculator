2b133b60ace0ee9a585e00951c225cefbe76c2a9
Mayank Pratap <mayank7860@gmail.com>
2016-06-15 17:53:25 +0530

    Everything working for positive numbers

--- js/calc.js
+++ js/calc.js
@@ -1,10 +1,13 @@
 $(document).ready(function(){
-   var ans=0;  // Stores result initially 0
-   // Attaching a callback function to click event
+   var ans="";  // Stores result initially 0
+   // Attaching a callback function to click even
+   var printed=0;  // Checks whther answer has been printed on screen.
+
+   $('#screen').val("");  // On page reload calculator screen must be empty
 
    $('#poweron').click(function(){
-
-      $('#screen').val("");
+      printed=0;
+      $('#screen').val("");   // $('#screen').empty()
       ans=0;
       console.log("Poweron");
       this.blur(); // Removes focus from button after clicking
@@ -22,7 +25,12 @@ $(document).ready(function(){
    });
 
    $('#modulus').click(function(){
+      if(printed==1){
 
+         $('#screen').val("");
+         printed=0;
+
+      }
       var content=$('#screen').val();
       $('#screen').val(content+'%');
       console.log("modulus");
@@ -31,6 +39,13 @@ $(document).ready(function(){
    });
 
    $('#divide').click(function(){
+
+     if(printed==1){
+
+        $('#screen').val("");
+        printed=0;
+
+     }
 
      var content=$('#screen').val();
      $('#screen').val(content+'/');
@@ -41,6 +56,13 @@ $(document).ready(function(){
 
    $('#number-7').click(function(){
 
+      if(printed==1){
+
+        $('#screen').val("");
+        printed=0;
+
+      }
+
       var content=$('#screen').val();
       console.log("Number 7");
       console.log(content);
@@ -50,6 +72,12 @@ $(document).ready(function(){
 
    $('#number-8').click(function(){
 
+     if(printed==1){
+
+        $('#screen').val("");
+        printed=0;
+
+     }
       var content=$('#screen').val();
       $('#screen').val(content+'8');
       console.log("Number 8");
@@ -59,7 +87,12 @@ $(document).ready(function(){
    });
 
    $('#number-9').click(function(){
+     if(printed==1){
 
+        $('#screen').val("");
+        printed=0;
+
+     }
      var content=$('#screen').val();
      $('#screen').val(content+'9');
      console.log("Number 9");
@@ -70,7 +103,10 @@ $(document).ready(function(){
    });
 
    $('#multiply').click(function(){
-
+      if(printed==1){
+        $('#screen').val("");
+        printed=0;
+      }
       var content=$('#screen').val();
       $('#screen').val(content+'*');
       console.log("Multiply");
@@ -79,7 +115,10 @@ $(document).ready(function(){
 
    });
    $('#number-4').click(function(){
-
+     if(printed==1){
+        $('#screen').val("");
+        printed=0;
+     }
      var content=$('#screen').val();
      $('#screen').val(content+'4');
      console.log("Number 4");
@@ -89,7 +128,10 @@ $(document).ready(function(){
    });
 
    $('#number-5').click(function(){
-
+     if(printed==1){
+        $('#screen').val("");
+        printed=0;
+     }
      var content=$('#screen').val();
      $('#screen').val(content+'5');
      console.log("Number 5");
@@ -99,7 +141,10 @@ $(document).ready(function(){
    });
 
    $('#number-6').click(function(){
-
+      if(printed==1){
+        $('#screen').val("");
+        printed=0;
+      }
       var content=$('#screen').val();
       $('#screen').val(content+'6');
       console.log("Number 6");
@@ -109,7 +154,10 @@ $(document).ready(function(){
    });
 
    $('#minus').click(function(){
-
+      if(printed==1){
+        $('#screen').val("");
+        printed=0;
+      }
       var content=$('#screen').val();
       $('#screen').val(content+'-');
       console.log("Number 7");
@@ -119,7 +167,10 @@ $(document).ready(function(){
    });
 
    $('#number-1').click(function(){
-
+      if(printed==1){
+        $('#screen').val("");
+        printed=0;
+      }
       var content=$('#screen').val();
       $('#screen').val(content+'1');
       console.log("Number 1");
@@ -129,7 +180,10 @@ $(document).ready(function(){
    });
 
    $('#number-2').click(function(){
-
+       if(printed==1){
+        $('#screen').val("");
+        printed=0;
+      }
       var content=$('#screen').val();
       $('#screen').val(content+'2');
       console.log("Number 2");
@@ -139,28 +193,36 @@ $(document).ready(function(){
    });
 
    $('#number-3').click(function(){
-
-      var content=$('#screen').val();
-      $('#screen').val(content+'3');
-      console.log("Number 3");
-      console.log(content);
-      this.blur();
+       if(printed==1){
+        $('#screen').val("");
+        printed=0;
+       }
+       var content=$('#screen').val();
+       $('#screen').val(content+'3');
+       console.log("Number 3");
+       console.log(content);
+       this.blur();
 
    });
 
    $('#plus').click(function(){
-
+     if(printed==1){
+        $('#screen').val("");
+        printed=0;
+     }
      var content=$('#screen').val();
      $('#screen').val(content+'+');
      console.log("Plus");
      console.log(content);
      this.blur();
 
-
    });
 
    $('#dot').click(function(){
-
+     if(printed==1){
+        $('#screen').val("");
+        printed=0;
+     }
      var content=$('#screen').val();
      $('#screen').val(content+'.');
      console.log("Dot");
@@ -170,7 +232,10 @@ $(document).ready(function(){
    });
 
    $('#zero').click(function(){
-
+     if(printed==1){
+        $('#screen').val("");
+        printed=0;
+     }
      var content=$('#screen').val();
      $('#screen').val(content+'0');
      console.log("Zero");
@@ -181,7 +246,10 @@ $(document).ready(function(){
    });
 
    $('#result').click(function(){
-
+     if(printed==1){
+        $('#screen').val("");
+        printed=0;
+     }
      var content=$('#screen').val();
      $('#screen').val(content+ans.toString());
      console.log("Ans");
@@ -190,15 +258,160 @@ $(document).ready(function(){
    });
 
    $('#equals').click(function(){
-
+      var checkZeroError=0;
       var content=$('#screen').val();
       console.log("Equals");
       console.log(content);
-      $('#screen').empty();
+      content=infixToPostfix(content);  // Converting infix mathematical expression to postfix
+      ans=evaluatePostfix(content); // Evaluating postfix expression.
+      function infixToPostfix(content){
+
+        var result=[];
+        var stack=[];
+
+        var pre=0;
+        var priority={'+':0,'-':0,'*':1,'/':1,'%':1};
+
+        var i;
+
+        for(i=0;i<content.length;++i){
+
+           // console.log(result.length);
+           //console.log(i);
+           //  console.log(stack.length);
+           var j=stack.length;
+
+           if(content[i]=='+' || content[i]=='-' || content[i]=='*' || content[i]=='/' || content[i]=='%'){
+             result.push(content.substr(pre,i-pre));  // Get operand before this operator
+
+           if(stack.length===0)
+              stack.push(content[i]);
+
+           else{
+
+             while(stack.length>0){
+
+                var temp=stack.pop();
+
+                if(priority[temp]<priority[content[i]]){
+
+                    stack.push(temp);
+                    break;
+                }  // close to if
+
+                else
+                    result.push(temp);
+
+
+               }  // close to while(!stack...)
+
+              stack.push(content[i]);
+
+          }  // close to else;
+
+
+          pre=i+1;
+
+        }  // close  to bahar waala if
+
+     }  // close to for  loop
+
+     result.push(content.substr(pre,i-pre));
+
+
+     while(stack.length>0){
+
+        var temp=stack.pop();
+        result.push(temp);
+
+      }
+
+      console.log(result.length);
+
+
+      return result;
+   }
+
+   function evaluatePostfix(content){
+
+     var stack=[];
+
+     for(var i=0;i<content.length;++i){
+       console.log(content[i]);
+
+     if(content[i]=='+' || content[i]=='-' || content[i]=='*' || content[i]=='/' || content[i]=='%'){
+
+         var operand1=stack.pop();
+         var operand2=stack.pop();
+
+         operand1=parseFloat(operand1);
+         operand2=parseFloat(operand2);
+
+         var result;
+
+         if(content[i]=='+')
+            result=operand1+operand2;
+         else if(content[i]=='-')
+            result=operand2-operand1;
+
+         else if(content[i]=='*')
+            result=operand1*operand2;
+         else if(content[i]=='/'){
+            // Place check for divide by zero error
+            if(operand1==0){
+
+                checkZeroError=1;
+                break;
+
+            }
+
+            result=operand2/operand1;
+
+         }
+         else if(content[i]=='%'){
+
+            // Place check for divide by zero error
+            if(operand1==0){
+
+               checkZeroError=1;
+               break;
+
+            }
+
+            result=operand2%operand1;
+         }
+
+         stack.push(result.toString());
+
+      }
+
+      else{
+
+         stack.push(content[i]);
+
+      }
+
+    }
+
+      var ans=stack.pop();
+      return ans;
+
+    }   // Close to function
+
+
+
+      console.log(ans);
+      printed=1;
+      $('#screen').val("");
+      if(checkZeroError==1)
+        $('#screen').val("Divide by zero kills.");
+      else
+        $('#screen').val(ans);
       this.blur();
 
-
    });
+
+
 
 
 
